e50e429e924cb022f4fe0572ad2375b26216f5ba
koolaash <73039811+koolaash@users.noreply.github.com>
2022-04-19 19:16:00 +0530

    roleinfo command

--- commands/USEFUL/roleinfo.js
+++ commands/USEFUL/roleinfo.js
@@ -11,31 +11,24 @@ module.exports = {
 
     async run(client, message, args) {
         await message.guild.members.fetch()
-        let roles = await message.guild.roles.fetch()
 
-        let role = message.mentions.roles.first() || roles.find(r => r.id === args[0])
-            || roles.find(r => r.name === args.join(" ")),
-            embed = new discord.MessageEmbed({
-                color: role.hexColor || client.embed.cm,
-                title: `${role.name}'s Information`,
+        let role = await message.mentions.roles.first() || await message.guild.roles.cache.find(r => r.id === args[0])
+            || await message.guild.roles.cache.find(r => r.name === args.join(" "))
 
-            })
-                .addField(`Role ID`, role.id, true)
-                .addField(`Role Mentionable`, role.mentionable ? 'Yes' : 'No', true)
-                .addField(`Role Color`, role.hexColor || 'Default', true)
-                .addField(`Role Display Saperately`, role.hoist ? 'Yes' : 'No', true)
-                //   .addField(`Role Position`, role.position)
-                .addField(`Role Members`, role.members.size || '0')
-                //   .addField(`Members`, role.members.map(m => m) || "None")
-                .addField(`Role Permissions`, `\`${role.permissions.toArray().join("', '") || "None"}\``)
 
-        let icon = role.iconURL
-        if (icon !== null) {
-            embed.setThumbnail(icon)
-        } else {
-            embed.setThumbnail(message.guild.iconURL({ dynamic: true }))
-        }
-        message.reply({ embeds: [embed] });
-        console.log(`${role.position}`)
+        const embed = new discord.MessageEmbed({
+            color: role.hexColor || client.embed.cm,
+            title: `${role.name}'s Information`,
+            description: `**Role ID : **${role.id}\n` +
+                `**Role Mentionable : **${role.mentionable ? 'Yes' : 'No'}\n` +
+                `**Role Color : **${role.hexColor || 'Default'}\n` +
+                `**Role Display Saperately : **${role.hoist ? 'Yes' : 'No'}\n` +
+                `**Role Position : **${role.position}\n` +
+                `**Role Members : **${role.members.size || '0'}\n` +
+                `**Role Permissions : **\n\`${role.permissions.toArray().join("', '") || "None"}\`\n` +
+                `**Members : **Use dump command to check members!`,
+            thumbnail: message.guild.iconURL({ dynamic: true })
+        })
+        return message.reply({ embeds: [embed] });
     }
 };
